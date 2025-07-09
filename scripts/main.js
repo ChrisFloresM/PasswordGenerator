040011693140612f7b66cb86d3dfd8b0592ee9f6
Christian <christian.mig.flmo@gmail.com>
2025-07-09 09:15:01 -0600

    Adding slider functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,32 @@
 /* Slider functionality */
 const slider = document.getElementById('pass-length');
-const output = document.querySelector('.slider-value');
-const sliderWrapper = document.querySelector('.main-app_slider-container');
+const sliderValue = document.querySelector('.slider-value');
 
 slider.addEventListener('input', sliderListener);
 
-function sliderListener() {
-	const maxValue = Number(this.max);
-	output.textContent = this.value;
+const passwordAttributes = {
+	passwordLen: 0,
+	includeUppercase: false,
+	includeLowercase: false,
+	includeNumbers: false,
+	includeSymbols: false
+}
 
-	const currentPercentage = (Number(this.value) / maxValue ) * 100;
+/* ================= Slider functionality ================= */
+function sliderListener() {
+	updateSliderUI(this);
+	passwordAttributes.passwordLen = Number(this.value);
+}
+
+function updateSliderUI(sliderEl) {
+	const value = sliderEl.value;
+	const maxValue = Number(sliderEl.max);
+	const currentPercentage = (Number(value) / maxValue ) * 100;
+
+	sliderValue.textContent = value;
 	slider.style.background = `linear-gradient(90deg, var(--green-200) ${currentPercentage}%, var(--grey-850) ${currentPercentage}%)`;
 }
+
+/* Testing crypto.getRandomValues */
+const buffer = new Uint32Array(1)
+console.log(window.crypto.getRandomValues(buffer));
